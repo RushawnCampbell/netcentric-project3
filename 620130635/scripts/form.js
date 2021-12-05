240aeee73faaf3ed86734a7e2c8d765373bdad7d
r11Sr <rs_burner2@outlook.com>
2021-12-05 18:04:46 -0500

    the form.js stuf

--- 620130635/scripts/form.js
+++ 620130635/scripts/form.js
@@ -4,12 +4,15 @@ window.addEventListener("load", (event)=> {
     let pollstat = document.querySelector(`input[name="pollstat"]`);
     let c1votes = document.querySelector(`input[name="c1votes"]`);
     let c2votes = document.querySelector(`input[name="c2votes"]`);
-    let reject = document.querySelector(`input[name="reject"]`)
+    let reject = document.querySelector(`input[name="reject"]`);
     let totalv = document.querySelector(`input[name="totalv"]`);
     let submitbtn = document.querySelector("button#submitbtn");
 
-    submitbtn.onclick = (event) =>{
-        event.preventDefault();
+
+    console.log(inputFields);
+
+    submitbtn.addEventListener('click',(event) =>{
+        // event.preventDefault();
 
         inputFields.forEach(inp => {
             inp.classList.remove("error");
@@ -28,5 +31,5 @@ window.addEventListener("load", (event)=> {
         if( (parseInt(c1votes.value)+ parseInt(c2votes.value) +parseInt(reject.value)) != parseInt(totalv.value) ){
             totalv.classList.add("error");
         }
-    }
+    });
 });
